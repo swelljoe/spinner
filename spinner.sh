da1d2744a8c132fce3b3fb10f394d7bc97d07844
Joe Cooper <joe@virtualmin.com>
2017-03-12 17:40:08 -0500

    Wrap it in function, scope vars, add config

--- spinner.sh
+++ spinner.sh
@@ -1,49 +1,59 @@
+# Config variables, set these after sourcing to change behavior.
+SPINNER_COLORNUM=2 # What color? Irrelevent if COLORCYCLE=1.
+SPINNER_COLORCYCLE=1 # Does the color cycle?
+SPINNER_DONEFILE="stopspinning" # Path/name of file to exit on.
+SPINNER_SYMBOLS="UNI_BRAILLE2" # Name of the variable containing the symbols.
 
-# Safest option are one of these. Doesn't need Unicode, at all.
-ASCII_LINE="/ - \\ \|"
-ASCII_PLUS="x +"
-ASCII_SPLAT='. o O @ \*'
-ASCII_BLINK="o -"
-ASCII_V="v < ^ >"
-ASCII_INFLATE=". o O o"
+spinner () {
+  # Safest option are one of these. Doesn't need Unicode, at all.
+  local ASCII_LINE="/ - \\ \|"
+  local ASCII_PLUS="x +"
+  local ASCII_SPLAT='. o O @ \*'
+  local ASCII_BLINK="o -"
+  local ASCII_V="v < ^ >"
+  local ASCII_INFLATE=". o O o"
 
-# Needs Unicode support in shell and terminal.
-# These are ordered most to least likely to be available, in my limited experience.
-UNI_BRAILLE="⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏"
-UNI_BRAILLE2="⣷ ⣯ ⣟ ⡿ ⢿ ⣻ ⣽ ⣾"
-UNI_BRAILLE2_COUNTER="⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷"
-UNI_BOUNCE="⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈"
-UNI_HIPPIE="☮ ✌ ☺ ♥"
-UNI_HANDS="☜ ☝ ☞ ☟"
-UNI_ARROW_ROT="➫ ➭ ➬ ➭"
-UNI_CARDS="♣ ♤ ♥ ♦"
-UNI_TRIANGLE="◢ ◣ ◤ ◥"
-UNI_SQUARE="◰ ◳ ◲ ◱"
-UNI_PIE="◴ ◷ ◶ ◵"
-UNI_CIRCLE="◐ ◓ ◑ ◒"
-UNI_QTR_CIRCLE="◜ ◝ ◞ ◟" 
+  # Needs Unicode support in shell and terminal.
+  # These are ordered most to least likely to be available, in my limited experience.
+  local UNI_BRAILLE="⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏"
+  local UNI_BRAILLE2="⣷ ⣯ ⣟ ⡿ ⢿ ⣻ ⣽ ⣾"
+  local UNI_BRAILLE2_COUNTER="⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷"
+  local UNI_BOUNCE="⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈"
+  local UNI_HIPPIE="☮ ✌ ☺ ♥"
+  local UNI_HANDS="☜ ☝ ☞ ☟"
+  local UNI_ARROW_ROT="➫ ➭ ➬ ➭"
+  local UNI_CARDS="♣ ♤ ♥ ♦"
+  local UNI_TRIANGLE="◢ ◣ ◤ ◥"
+  local UNI_SQUARE="◰ ◳ ◲ ◱"
+  local UNI_PIE="◴ ◷ ◶ ◵"
+  local UNI_CIRCLE="◐ ◓ ◑ ◒"
+  local UNI_QTR_CIRCLE="◜ ◝ ◞ ◟" 
 
-SYMBOLS=$UNI_BRAILLE2
-COLORNUM=3
-NORMAL=$(tput sgr0)
-COLORCYCLE=0
+  local NORMAL=$(tput sgr0)
 
-while :; do
-  tput civis
-  for c in ${SYMBOLS}; do
-    if [ $COLORCYCLE -eq 1 ]; then
-      if [ $COLORNUM -eq 7 ]; then
-        COLORNUM=1
-      else
-        COLORNUM=$((COLORNUM+1))
+  eval SYMBOLS=\$${SPINNER_SYMBOLS}
+
+  while :; do
+    tput civis
+    for c in ${SYMBOLS}; do
+      if [ $SPINNER_COLORCYCLE -eq 1 ]; then
+        if [ $SPINNER_COLORNUM -eq 7 ]; then
+          SPINNER_COLORNUM=1
+        else
+          SPINNER_COLORNUM=$((SPINNER_COLORNUM+1))
+        fi
       fi
-    fi
-    COLOR=$(tput setaf ${COLORNUM})
-    tput sc
-    env printf "${COLOR}${c}${NORMAL}"
-    tput rc
-    [ -f results ] && { printf '\n'; break 2; }
-    env sleep .4
+      local COLOR=$(tput setaf ${SPINNER_COLORNUM})
+      tput sc
+      env printf "${COLOR}${c}${NORMAL}"
+      tput rc
+      if [ -f "${SPINNER_DONEFILE}" ]; then
+        printf '\n'
+	rm ${SPINNER_DONEFILE}
+	break 2
+      fi
+      env sleep .4
+    done
   done
   tput cnorm
-done
+}
