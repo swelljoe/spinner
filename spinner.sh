b2a70c557dfb6d3f9a4d494a1a1a6750f228fe38
Joe Cooper <joe@virtualmin.com>
2017-03-14 00:00:53 -0500

    Lots of new spinners

--- spinner.sh
+++ spinner.sh
@@ -2,11 +2,12 @@
 SPINNER_COLORNUM=2 # What color? Irrelevent if COLORCYCLE=1.
 SPINNER_COLORCYCLE=1 # Does the color cycle?
 SPINNER_DONEFILE="stopspinning" # Path/name of file to exit on.
-SPINNER_SYMBOLS="UNI_BRAILLE2" # Name of the variable containing the symbols.
+SPINNER_SYMBOLS="UNI_DOTS2" # Name of the variable containing the symbols.
+SPINNER_CLEAR=1 # Blank the line when done.
 
 spinner () {
   # Safest option are one of these. Doesn't need Unicode, at all.
-  local ASCII_LINE="/ - \\ \|"
+  local ASCII_LINE="/ - \\ |"
   local ASCII_PLUS="x +"
   local ASCII_BLINK="o -"
   local ASCII_V="v < ^ >"
@@ -14,19 +15,36 @@ spinner () {
 
   # Needs Unicode support in shell and terminal.
   # These are ordered most to least likely to be available, in my limited experience.
-  local UNI_BRAILLE="⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏"
-  local UNI_BRAILLE2="⣷ ⣯ ⣟ ⡿ ⢿ ⣻ ⣽ ⣾"
-  local UNI_BRAILLE2_COUNTER="⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷"
-  local UNI_BOUNCE="⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈"
+  local UNI_DOTS="⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏"
+  local UNI_DOTS2="⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷"
+  local UNI_DOTS3="⣷ ⣯ ⣟ ⡿ ⢿ ⣻ ⣽ ⣾"
+  local UNI_DOTS4="⠋ ⠙ ⠚ ⠞ ⠖ ⠦ ⠴ ⠲ ⠳ ⠓"
+  local UNI_DOTS5="⠄ ⠆ ⠇ ⠋ ⠙ ⠸ ⠰ ⠠ ⠰ ⠸ ⠙ ⠋ ⠇ ⠆"
+  local UNI_DOTS6="⠋ ⠙ ⠚ ⠒ ⠂ ⠂ ⠒ ⠲ ⠴ ⠦ ⠖ ⠒ ⠐ ⠐ ⠒ ⠓ ⠋"
+  local UNI_DOTS7="⠁ ⠉ ⠙ ⠚ ⠒ ⠂ ⠂ ⠒ ⠲ ⠴ ⠤ ⠄ ⠄ ⠤ ⠴ ⠲ ⠒ ⠂ ⠂ ⠒ ⠚ ⠙ ⠉ ⠁"
+  local UNI_DOTS8="⠈ ⠉ ⠋ ⠓ ⠒ ⠐ ⠐ ⠒ ⠖ ⠦ ⠤ ⠠ ⠠ ⠤ ⠦ ⠖ ⠒ ⠐ ⠐ ⠒ ⠓ ⠋ ⠉ ⠈"
+  local UNI_DOTS9="⠁ ⠁ ⠉ ⠙ ⠚ ⠒ ⠂ ⠂ ⠒ ⠲ ⠴ ⠤ ⠄ ⠄ ⠤ ⠠ ⠠ ⠤ ⠦ ⠖ ⠒ ⠐ ⠐ ⠒ ⠓ ⠋ ⠉ ⠈ ⠈"
+  local UNI_DOTS10="⢹ ⢺ ⢼ ⣸ ⣇ ⡧ ⡗ ⡏"
+  local UNI_DOTS11="⢄ ⢂ ⢁ ⡁ ⡈ ⡐ ⡠"
+  local UNI_DOTS12="⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈"
+  local UNI_BOUNCE="⠁ ⠂ ⠄ ⠂"
+  local UNI_PIPES="┤ ┘ ┴ └ ├ ┌ ┬ ┐"
   local UNI_HIPPIE="☮ ✌ ☺ ♥"
   local UNI_HANDS="☜ ☝ ☞ ☟"
   local UNI_ARROW_ROT="➫ ➭ ➬ ➭"
   local UNI_CARDS="♣ ♤ ♥ ♦"
   local UNI_TRIANGLE="◢ ◣ ◤ ◥"
   local UNI_SQUARE="◰ ◳ ◲ ◱"
+  local UNI_BOX_BOUNCE="▖ ▘ ▝ ▗"
   local UNI_PIE="◴ ◷ ◶ ◵"
   local UNI_CIRCLE="◐ ◓ ◑ ◒"
   local UNI_QTR_CIRCLE="◜ ◝ ◞ ◟" 
+
+  # Bigger spinners and progress type bars; takes more space.
+  local WIDE_ASCII_PROG="[>----] [=>---] [==>--] [===>-] [====>] [----<] [---<=] [--<==] [-<===] [<====]"
+  local WIDE_ASCII_PROPELLER="[|####] [#/###] [##-##] [###\\#] [####|] [###\\#] [##-##] [#/###]"
+  local WIDE_ASCII_SNEK="[>----] [~>---] [~~>--] [~~~>-] [~~~~>] [----<] [---<~] [--<~~] [-<~~~] [<~~~~]"
+  local WIDE_UNI_GREYSCALE="░░░░░░░ ▒░░░░░░ ▒▒░░░░░ ▒▒▒░░░░ ▒▒▒▒░░░ ▒▒▒▒▒░░ ▒▒▒▒▒▒░ ▒▒▒▒▒▒▒ ▒▒▒▒▒▒░ ▒▒▒▒▒░░ ▒▒▒▒░░░ ▒▒▒░░░░ ▒▒░░░░░ ▒░░░░░░ ░░░░░░░"
 
   local SPINNER_NORMAL=$(tput sgr0)
 
@@ -47,14 +65,17 @@ spinner () {
       env printf "${COLOR}${c}${SPINNER_NORMAL}"
       tput rc
       if [ -f "${SPINNER_DONEFILE}" ]; then
-        tput el
+        if [ ${SPINNER_CLEAR} -eq 1 ]; then
+          tput el
+        fi
 	rm ${SPINNER_DONEFILE}
 	break 2
       fi
       # This is questionable. sleep with fractional seconds is not
       # always available, but seems to not break things, when not.
-      env sleep .4
+      env sleep .2
     done
   done
   tput cnorm
+  return 0
 }
--- tests/spinnerette.sh
+++ tests/spinnerette.sh
@@ -2,20 +2,43 @@
 
 . ../spinner.sh
 
-printf "Spinner with default settings for five seconds: "
-spinner &
-sleep 5
-touch "stopspinning"
-printf "\n"
-sleep 1
-
 SPINNER_COLORCYCLE=0
 SPINNER_COLORNUM=2
 SPINNER_DONEFILE="donespinning"
-SPINNER_SYMBOLS="ASCII_PLUS"
-printf "Spinner with some custom settings for five seconds: "
-spinner &
-sleep 5
-touch "donespinning"
-printf "\n"
-sleep 1
+
+run_test () {
+  SPINNER_SYMBOLS=$1
+  # Auto cycle through colors
+  if [ $SPINNER_COLORNUM -eq 7 ]; then
+    SPINNER_COLORNUM=1
+  else
+    SPINNER_COLORNUM=$((SPINNER_COLORNUM+1))
+  fi
+
+  printf "${SPINNER_SYMBOLS}: "
+  spinner &
+  sleep 5
+  touch "donespinning"
+  sleep 1
+  printf "\n"
+}
+
+echo "Single character ASCII spinners"
+for s in ASCII_LINE ASCII_PLUS ASCII_BLINK ASCII_INFLATE ASCII_V; do
+  run_test ${s}
+done
+
+echo "Single character Unicode spinners"
+for s in UNI_DOTS UNI_DOTS2 UNI_DOTS3 UNI_DOTS4 UNI_DOTS5 UNI_DOTS6 UNI_DOTS7 UNI_DOTS8 UNI_DOTS9 UNI_DOTS10 UNI_DOTS11 UNI_DOTS12 UNI_BOUNCE UNI_PIPES UNI_HIPPIE UNI_HANDS UNI_ARROW_ROT; do
+  run_test ${s}
+done
+
+echo "Wide ASCII spinners"
+for s in WIDE_ASCII_PROG WIDE_ASCII_PROPELLER WIDE_ASCII_SNEK; do
+  run_test ${s}
+done
+
+echo "Wide Unicode spinners"
+for s in WIDE_UNI_GREYSCALE; do
+  run_test ${s}
+done
